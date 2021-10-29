fe0b0ec04ec5ef8669b938cbd19f764b9a2dfabe
Hyunji Cho <sentiersg@gmail.com>
2021-10-29 18:12:59 +0900

    get bootstrap template

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-
 function onHttpStart() {
     console.log("Express http server listening on " + HTTP_PORT);
   }
 
 app.use(express.static("./assets/"));
-
-
 
 // setup a 'route' to listen on the default url path
 app.get("/", function(req,res){
@@ -19,3 +16,7 @@ app.get("/", function(req,res){
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
+
+
+// bootstrap template
+// https://themewagon.com/themes/free-bootstrap-4-html5-admin-dashboard-template-plus-admin/
