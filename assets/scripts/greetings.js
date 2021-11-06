a732fc0b53c09c40ae44d0b64f68aeedc523d089
Moon <moonsc014@gmail.com>
2021-11-06 14:13:15 +0900

    Add scripts for exercise pages

---
+++ assets/scripts/greetings.js
@@ -0,0 +1,13 @@
+const kor = document.querySelector('.kor-sentence');
+const situation = document.querySelector('.situation');
+const eng = document.querySelector('.eng-description');
+const url = 'http://localhost:8080/greeting.json';
+
+let idx = 0;
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    kor.innerHTML = data[idx].한국어;
+    situation.innerHTML = data[idx].소분류;
+    eng.innerHTML = data[idx].영어;
+  });
